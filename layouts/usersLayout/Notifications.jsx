3240bf1cc807fe7af83496d4cea830a0e9af7930
inedumozey <inedumozey@gmail.com>
2022-08-11 18:58:07 -0700

    updated

--- layouts/usersLayout/Notifications.jsx
+++ layouts/usersLayout/Notifications.jsx
@@ -1,17 +1,25 @@
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'; 
 import styled from 'styled-components'
 import { useRouter } from 'next/router';
+import {useSelector, useDispatch} from 'react-redux';
 import { getNotif } from '../../redux/admin/notifications';
-import {useDispatch} from 'react-redux'
-import { useEffect } from 'react';
+import { useEffect, useState} from 'react';
+
 
 export default function Notifications({notificationId}) {
   const router = useRouter()
+  const state = useSelector(state=>state);
   const dispatch = useDispatch();
+  const {notif} = state.notifications;
+  const [allNotificationId, setAllNotificationId] = useState([])
   
   const handleShowNotif =()=>{
     router.push('/dashboard/notifications')
   }
+
+  useEffect(()=>{
+    setAllNotificationId(notif.data || [])
+  }, [notif])
 
   useEffect(()=>{
     dispatch(getNotif())
@@ -21,7 +29,7 @@ export default function Notifications({notificationId}) {
   return (
     <NotificationWrapper notificationId={notificationId}>
         <NotificationsNoneIcon title="Toggle notifications panel" style={{fontSize: '1.5rem'}} onClick={handleShowNotif}/>
-         <span className="alert" onClick={handleShowNotif}>{notificationId.length}</span>
+         <span className="alert" onClick={handleShowNotif}>{notificationId.length < 1 ?allNotificationId.length : notificationId.length}</span>
     </NotificationWrapper>
   )
 }
@@ -33,8 +41,8 @@ const NotificationWrapper = styled.div`
     z-index: 1000;
 
     .alert{
-        width: 18px;
-        height: 18px;
+        min-width: 18px;
+        min-height: 18px;
         display: flex;
         justify-content: center;
         align-items; center;
@@ -42,7 +50,7 @@ const NotificationWrapper = styled.div`
         position: absolute;
         top: -1px;
         font-weight: bold;
-        font-size: .7rem;
+        font-size: .6rem;
         right: -4px;
         background: ${({notificationId})=>notificationId.length > 0 ? 'red' : '#888'};
     }
