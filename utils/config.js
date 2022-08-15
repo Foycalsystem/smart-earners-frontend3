c5049fb12f7998daca788b3b003acc033414253b
inedumozey <inedumozey@gmail.com>
2022-08-15 08:39:22 -0700

    changed api url from heroku to digitalocean

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
-export const FRONTEND_BASE_URL = 'http://localhost:3000'
-// export const BACKEND_BASE_URL = 'http://localhost:4000'
-export const BACKEND_BASE_URL_LATER = 'https://api.teamsmartearners.com/'
-export const BACKEND_BASE_URL = 'https://smartearnersbackend.herokuapp.com/'
+// these are not usefull for now, 
+
+export const BACKEND_BASE_URL = 'https://api.teamsmartearners.com/'
+export const FRONTEND_BASE_URL = 'https://sea-turtle-app-qmavg.ondigitalocean.app'
