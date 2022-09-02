ef096a3f12d8f96cc37bb830b2930fd55591d51c
inedumozey <inedumozey@gmail.com>
2022-09-01 23:07:16 -0700

    updated resolvedPosition in utils

--- utils/resolvePosition.js
+++ utils/resolvePosition.js
@@ -1,13 +1,24 @@
 const position = (n)=>{
-    if(`${n}` === '1' || `${n}` === '21' || `${n}` === '31' || `${n}` === '41'  || `${n}` === '51' || `${n}` === '61' || `${n}` === '71' || `${n}` === '81' || `${n}` === '91'){
+    //get the last always
+    const num = n.toString()
+    const lastDigit = +(num[num.length-1])
+
+    // check if lastDigit is 1 and n is not 11, add st
+    if(lastDigit===1 && n !== 11){
         return 'st'
     }
-    else if(`${n}`.includes('2')){
+
+    // check if lastDigit is 2 and n is not 12, add nd
+    else if(lastDigit===2 && n !== 12){
         return 'nd'
     }
-    else if(`${n}`.includes('3')){
+
+    // check if lastDigit is 3 and n is not 13, add rd
+    else if(lastDigit===3 && n !== 13){
         return 'rd'
     }
+
+    // any other number, add th
     else{
         return 'th'
     }
